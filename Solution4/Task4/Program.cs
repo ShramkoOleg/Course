16db63e0997b709c0fa4f4b5048a47c443a6c105
ShramkoOleg <shramko_oa@mail.ru>
2024-03-03 18:24:05 +0300

    Задание 1 из темы 4.

--- Solution4/Task4/Program.cs
+++ Solution4/Task4/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task4
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int N, sq;
+            int sum = 0;
+            int i;
+            Console.WriteLine("Введите число.");
+            N = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= N; i++)
+            {
+                sum = sum + (2 * i - 1);
+                Console.WriteLine("Текущее значение суммы составляет {0}.", sum);
+            }
+            sq = sum;
+            Console.WriteLine("Квадрат числа равен {0}.", sq);
+            Console.ReadKey();
         }
     }
 }
