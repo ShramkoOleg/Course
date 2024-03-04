9445b8f802a9e2618be492f962c0e78d7a8f7e56
ShramkoOleg <shramko_oa@mail.ru>
2024-03-04 16:41:53 +0300

    Добавлено: тема 7, задания 1 и 2.

---
+++ Lesson7/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double S1, S2;
+            Console.WriteLine("Введите длины трёх сторон первого треугольника.");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            double c1 = Convert.ToDouble(Console.ReadLine());
+            S1 = GetSquare(a1, b1, c1);
+            Console.WriteLine("Введите длины трёх сторон второго треугольника.");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            double c2 = Convert.ToDouble(Console.ReadLine());
+            S2 = GetSquare(a2, b2, c2);
+
+            if (S1 > S2)
+            {
+                Console.WriteLine("Площадь первого треугольника больше.");
+            }
+            else
+            {
+                if (S1 < S2)
+                {
+                    Console.WriteLine("Площадь второго треугольника больше.");
+                }
+                else
+                {
+                    Console.WriteLine("Площади треугольников равны.");
+                }
+            }
+            Console.ReadKey();
+        }
+        static double GetSquare(double a, double b, double c)
+        {
+            double p, S;
+            p = (a + b + c) / 2.0;
+            S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return S;
+        }
+    }
+}
---
+++ Lesson7/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, S, V;
+            Console.WriteLine("Введите длину ребра куба.");
+            a = Convert.ToDouble(Console.ReadLine());
+            GetParamsCube(a, out S, out V);
+            Console.WriteLine("Площадь поверхности куба равна {0}.", S);
+            Console.WriteLine("Объём куба равен {0}.", V);
+            Console.ReadKey();
+        }
+        static void GetParamsCube(double a, out double S, out double V)
+        {
+            S = 6 * a * a;
+            V = a * a * a;
+        }
+    }
+}
