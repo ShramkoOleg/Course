8529622073cd3e0c17f878a43fd23854c3729e5e
ShramkoOleg <shramko_oa@mail.ru>
2024-03-05 13:19:02 +0300

    Добавлено: тема 10, задание 1.

---
+++ Lesson10/Task1/Angle.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public int incorrect = 777;
+        public bool isCorrect = true;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value == incorrect)
+                {
+                    isCorrect = false;
+                }
+                else
+                {
+                    gradus = value % 360;
+                }
+            }
+        }
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value == incorrect)
+                {
+                    isCorrect = false;
+                }
+                else
+                {
+                    Gradus = Gradus + value / 60;
+                    min = value % 60;
+                }
+            }
+        }
+
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value == incorrect)
+                {
+                    isCorrect = false;
+                }
+                else
+                {
+                    Min = Min + value / 60;
+                    sec = value % 60;
+                }
+            }
+        }
+
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+
+        public double ToRadian()
+        {
+            return (gradus + min / 60.0 + sec / 3600.0) / 180.0 * Math.PI;
+        }
+    }
+}
---
+++ Lesson10/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radian;
+            Console.WriteLine("Введите целочисленные значения для градусов, минут, секунд угла.");
+            int gr = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            Angle angle = new Angle(gr, m, s);
+            if (angle.isCorrect == false)
+            {
+                Console.WriteLine("Введено некорректное значение. Программа окончена.");
+                Console.ReadKey();
+            }
+            else
+            {
+                radian = angle.ToRadian();
+                Console.WriteLine("Угол в радианах равен {0}", radian);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
