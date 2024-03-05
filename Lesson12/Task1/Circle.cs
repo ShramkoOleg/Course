b22bc8388cc5a26e9d70634c34783f891101e04f
ShramkoOleg <shramko_oa@mail.ru>
2024-03-05 15:10:51 +0300

    Добавлено: тема 12, задание 1.

---
+++ Lesson12/Task1/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    static class Circle
+    {
+        static public double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public string CheckPoint(double r, double x0, double y0, double x, double y)
+        {
+            if ((x - x0) * (x - x0) + (y - y0) * (y - y0) > r * r)
+            {
+                return "Точка не принадлежит кругу.";
+            }
+            else 
+            {
+                return "Точка принадлежит кругу.";
+            }
+        }
+    }
+}
---
+++ Lesson12/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, x0, y0, x, y;
+            double L, S;
+            Console.WriteLine("Введите радиус окружности.");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты центра.");
+            x0 = Convert.ToDouble(Console.ReadLine());
+            y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки.");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+
+            L = Circle.GetLength(r);
+            Console.WriteLine("Длина окружности равна {0}.", L);
+            S = Circle.GetSquare(r);
+            Console.WriteLine("Площадь круга равна {0}.", S);
+            Console.WriteLine(Circle.CheckPoint(r, x0, y0, x, y));
+            Console.ReadKey();
+        }
+    }
+}
