5b010cafa804a62081602274856eae2492e3e724
ShramkoOleg <shramko_oa@mail.ru>
2024-03-05 14:04:53 +0300

    Добавлено: тема 11, задание 1.

---
+++ Lesson11/Task1/Linear.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct Linear
+    {
+        double k;
+        double b;
+
+        public Linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            if (k == 0)
+            {
+                return "Коэффициент k = 0. Бесконечное множество решений.";
+            }
+            else
+            {
+                double r;
+                r = -b / k;
+                return $"Решение: {r}.";
+            }
+        }
+    }
+}
---
+++ Lesson11/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите k и b.");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Linear linear = new Linear(k, b);
+            Console.WriteLine(linear.Root());
+            Console.ReadKey();
+        }
+    }
+}
