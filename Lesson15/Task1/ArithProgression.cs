ca9703dd52aaa844a707a63b5c157d7bb32e4b1f
ShramkoOleg <shramko_oa@mail.ru>
2024-03-06 15:14:37 +0300

    Добавлено: тема 15, задание 1.

---
+++ Lesson15/Task1/ArithProgression.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class ArithProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int getNext()
+        {
+            currentValue = currentValue + step;
+            return currentValue;
+        }
+
+        public void setStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+}
---
+++ Lesson15/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();
+            Console.WriteLine("Арифметическая прогрессия.");
+            Console.WriteLine("Введите начальное целочисленное значение."); 
+            arith.setStart(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Введите целочисленный положительный шаг.");
+            arith.setStep(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Следующее значение равно {0}.", arith.getNext());
+            Console.WriteLine("Следующее значение равно {0}.", arith.getNext());
+            Console.WriteLine("Следующее значение равно {0}.", arith.getNext());
+            arith.reset();
+            Console.WriteLine("Произошёл сброс до начального значения.");
+            Console.WriteLine("Следующее значение равно {0}.", arith.getNext());
+            Console.WriteLine("Следующее значение равно {0}.", arith.getNext());
+            Console.ReadKey();
+
+            GeomProgression geom = new GeomProgression();
+            Console.WriteLine("Геометрическая прогрессия.");
+            Console.WriteLine("Введите начальное целочисленное значение.");
+            geom.setStart(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Введите целочисленный положительный шаг (множитель).");
+            geom.setStep(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Следующее значение равно {0}.", geom.getNext());
+            Console.WriteLine("Следующее значение равно {0}.", geom.getNext());
+            Console.WriteLine("Следующее значение равно {0}.", geom.getNext());
+            geom.reset();
+            Console.WriteLine("Произошёл сброс до начального значения.");
+            Console.WriteLine("Следующее значение равно {0}.", geom.getNext());
+            Console.WriteLine("Следующее значение равно {0}.", geom.getNext());
+            Console.ReadKey();
+        }
+    }
+}
