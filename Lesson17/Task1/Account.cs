f59c9fb4831c72802a213f6b5cdbd7b0efeab8cf
ShramkoOleg <shramko_oa@mail.ru>
2024-03-13 13:06:05 +0300

    Добавлено: тема 17, задание 1.

---
+++ Lesson17/Task1/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Account<T>
+    {
+        private T Num { get; set; }
+        private double Balance { get; set; }
+        private string Name { get; set; }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Номер счёта: {0}. Имя владельца счёта: {1}. Баланс: {2}.", Num, Name, Balance);
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счёта.");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите имя.");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите баланс.");
+            Balance = Convert.ToDouble(Console.ReadLine());
+        }
+
+    }
+}
---
+++ Lesson17/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<int> account1 = new Account<int>();
+            account1.Input();
+
+            Account<string> account2 = new Account<string>();
+            account2.Input();
+
+            account1.GetInfo();
+
+            account2.GetInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
