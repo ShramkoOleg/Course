21146e02b292a5fe8b1f8a89d820a96306824a26
ShramkoOleg <shramko_oa@mail.ru>
2024-03-14 16:05:16 +0300

    Добавлено: тема 22, задание 1.

---
+++ Lesson22/Task1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива.");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Func<object, int[]> func1 = new Func<object, int[]>(GetArray);
+            Task<int[]> task1 = new Task<int[]>(func1, n);
+
+            Func<Task<int[]>, int> func2 = new Func<Task<int[]>, int>(GetSum);
+            Task<int> task2 = task1.ContinueWith<int>(func2);
+
+            Func<Task<int[]>, int> func3 = new Func<Task<int[]>, int>(GetMax);
+            Task<int> task3 = task1.ContinueWith<int>(func3);
+
+            task1.Start();
+            Console.ReadKey();
+        }
+
+        static int[] GetArray(object a)
+        {
+            int n = (int)a;
+            int[] array = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(0, 10);
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            return array;
+        }
+
+        static int GetSum(Task<int[]> task)
+        {
+            int[] array = task.Result;
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            Console.WriteLine("Сумма: {0}.", sum);
+            return sum;
+        }
+
+        static int GetMax(Task<int[]> task)
+        {
+            int[] array = task.Result;
+            int max = array [0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine("Максимальный элемент: {0}.", max);
+            return max;
+        }
+    }
+}
